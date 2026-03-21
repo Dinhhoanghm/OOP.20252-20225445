e5fec7c457cc0d177c6ee450b6d75eae9ee36177
Dinhhoanghm <dodinhhoanghm@example.com>
2026-03-21 08:15:39 +0700

    feat: Add Constructors and Getters/Setters

--- src/main/java/vn/oop/Homework04/BankAccount.java
+++ src/main/java/vn/oop/Homework04/BankAccount.java
@@ -4,7 +4,41 @@ public class BankAccount {
 
     public static final double MINIMUM_BALANCE = 50000.0;
     public static final double TRANSFER_FEE_RATE = 0.02;
+
     private String accountNumber;
     private String ownerName;
     private double balance;
+
+    public BankAccount(String accountNumber, String ownerName, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = initialBalance;
+    }
+
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
 }
