fee9b01af8ae8c5a64e1b4e26ebd89dd890ee5fd
Dinhhoanghm <dodinhhoanghm@example.com>
2026-03-21 08:02:35 +0700

    init: Initialize HW04 folder and BankAccount class properties

---
+++ src/main/java/vn/oop/Homework04/BankAccount.java
@@ -0,0 +1,10 @@
+package vn.oop.Homework04;
+
+public class BankAccount {
+
+    public static final double MINIMUM_BALANCE = 50000.0;
+    public static final double TRANSFER_FEE_RATE = 0.02;
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+}
